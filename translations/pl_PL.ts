40168ddfa07ff46b32fc00b93b7ff4898ff46cab
Igor Ordecha <igor.ordecha@gmail.com>
2021-06-14 21:15:43 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: CutefishOS/Launcher
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/launcher/pl/

--- translations/pl_PL.ts
+++ translations/pl_PL.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/launcher.cpp" line="50"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation>Launcher</translation>
     </message>
 </context>
 <context>
@@ -14,12 +14,12 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="91"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Otwórz</translation>
     </message>
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="96"/>
         <source>Uninstall</source>
-        <translation type="unfinished"></translation>
+        <translation>Odinstaluj</translation>
     </message>
 </context>
 <context>
@@ -27,12 +27,12 @@
     <message>
         <location filename="../qml/main.qml" line="174"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Szukaj</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="260"/>
         <source>Not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie znaleziono</translation>
     </message>
 </context>
 </TS>
