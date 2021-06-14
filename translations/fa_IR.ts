a5b8a86aa6b54b1db10fa501ce9b58324b69bb8c
Ali Nabipour <eyenpix@gmail.com>
2021-06-14 20:02:57 +0000

    Translated using Weblate (Persian)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: CutefishOS/Launcher
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/launcher/fa/

--- translations/fa_IR.ts
+++ translations/fa_IR.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/launcher.cpp" line="50"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation>لانچر</translation>
     </message>
 </context>
 <context>
@@ -14,12 +14,12 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="91"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>باز کردن</translation>
     </message>
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="96"/>
         <source>Uninstall</source>
-        <translation type="unfinished"></translation>
+        <translation>حذف کردن</translation>
     </message>
 </context>
 <context>
@@ -27,12 +27,12 @@
     <message>
         <location filename="../qml/main.qml" line="174"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>جستجو</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="260"/>
         <source>Not found</source>
-        <translation type="unfinished"></translation>
+        <translation>پیدا نشد</translation>
     </message>
 </context>
 </TS>
