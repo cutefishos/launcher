a54d5db28598beee94c522981217770e3ebef6b1
Hosted Weblate <hosted@weblate.org>
2021-06-23 07:58:47 +0200

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translated using Weblate (Somali)
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Co-authored-by: Beendiid <beendiid@mail.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Luis Pérez <luis.perez@protonmail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/launcher/es/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/launcher/so/
    Translation: CutefishOS/Launcher

--- translations/so.ts
+++ translations/so.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/launcher.cpp" line="50"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation>Fure</translation>
     </message>
 </context>
 <context>
@@ -14,12 +14,12 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="91"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Fur</translation>
     </message>
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="96"/>
         <source>Uninstall</source>
-        <translation type="unfinished"></translation>
+        <translation>Ka saar</translation>
     </message>
 </context>
 <context>
@@ -27,12 +27,12 @@
     <message>
         <location filename="../qml/main.qml" line="174"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Raadi</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="260"/>
         <source>Not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Lama helin</translation>
     </message>
 </context>
 </TS>
